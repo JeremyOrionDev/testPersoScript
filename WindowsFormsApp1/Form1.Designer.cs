be4b411c0741046680d570027bc0b9ef8920f60c
jeremyorionDev <jeremy9315@hotmail.fr>
2017-12-13 17:01:43 +0100

    Modification pour cartes calypso Cards

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,112 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboProtocols = new System.Windows.Forms.ComboBox();
             this.cbReaders = new System.Windows.Forms.ComboBox();
-            this.lblShareMode = new System.Windows.Forms.Label();
-            this.comboShareMode = new System.Windows.Forms.ComboBox();
             this.txBxReponse = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txBxATR = new System.Windows.Forms.TextBox();
+            this.btnDF = new System.Windows.Forms.Button();
+            this.lblSelectReader = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnGetMemory = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 242);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(56, 13);
-            this.label6.TabIndex = 4;
-            this.label6.Text = "Card Type";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 207);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 13);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Card UID";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 169);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Card ATR";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 139);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Reponse";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 105);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Protocol";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Location = new System.Drawing.Point(46, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "Liste lecteurs";
             // 
-            // comboProtocols
-            // 
-            this.comboProtocols.FormattingEnabled = true;
-            this.comboProtocols.Location = new System.Drawing.Point(81, 102);
-            this.comboProtocols.Name = "comboProtocols";
-            this.comboProtocols.Size = new System.Drawing.Size(191, 21);
-            this.comboProtocols.TabIndex = 2;
-            this.comboProtocols.SelectedIndexChanged += new System.EventHandler(this.comboProtocols_SelectedIndexChanged);
-            // 
             // cbReaders
             // 
             this.cbReaders.FormattingEnabled = true;
-            this.cbReaders.Location = new System.Drawing.Point(81, 36);
+            this.cbReaders.Location = new System.Drawing.Point(115, 36);
             this.cbReaders.Name = "cbReaders";
             this.cbReaders.Size = new System.Drawing.Size(191, 21);
             this.cbReaders.TabIndex = 3;
-            // 
-            // lblShareMode
-            // 
-            this.lblShareMode.AutoSize = true;
-            this.lblShareMode.Location = new System.Drawing.Point(12, 74);
-            this.lblShareMode.Name = "lblShareMode";
-            this.lblShareMode.Size = new System.Drawing.Size(62, 13);
-            this.lblShareMode.TabIndex = 8;
-            this.lblShareMode.Text = "ShareMode";
-            // 
-            // comboShareMode
-            // 
-            this.comboShareMode.FormattingEnabled = true;
-            this.comboShareMode.Location = new System.Drawing.Point(81, 71);
-            this.comboShareMode.Name = "comboShareMode";
-            this.comboShareMode.Size = new System.Drawing.Size(191, 21);
-            this.comboShareMode.TabIndex = 2;
-            this.comboShareMode.SelectedValueChanged += new System.EventHandler(this.comboShareMode_SelectedValueChanged);
+            this.cbReaders.SelectedIndexChanged += new System.EventHandler(this.cbReaders_SelectedIndexChanged);
             // 
             // txBxReponse
             // 
-            this.txBxReponse.Location = new System.Drawing.Point(81, 139);
+            this.txBxReponse.Location = new System.Drawing.Point(115, 108);
             this.txBxReponse.Multiline = true;
             this.txBxReponse.Name = "txBxReponse";
             this.txBxReponse.Size = new System.Drawing.Size(191, 24);
@@ -141,30 +69,96 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(295, 36);
+            this.button1.Location = new System.Drawing.Point(312, 34);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 11;
-            this.button1.Text = "button1";
+            this.button1.Text = "Connecte";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Card ATR";
+            // 
+            // txBxATR
+            // 
+            this.txBxATR.Location = new System.Drawing.Point(115, 75);
+            this.txBxATR.Name = "txBxATR";
+            this.txBxATR.ReadOnly = true;
+            this.txBxATR.Size = new System.Drawing.Size(191, 20);
+            this.txBxATR.TabIndex = 13;
+            // 
+            // btnDF
+            // 
+            this.btnDF.Location = new System.Drawing.Point(49, 109);
+            this.btnDF.Name = "btnDF";
+            this.btnDF.Size = new System.Drawing.Size(51, 23);
+            this.btnDF.TabIndex = 14;
+            this.btnDF.Text = "Get MF";
+            this.btnDF.UseVisualStyleBackColor = true;
+            this.btnDF.Click += new System.EventHandler(this.btn_Get_MF_Click);
+            // 
+            // lblSelectReader
+            // 
+            this.lblSelectReader.AutoSize = true;
+            this.lblSelectReader.Location = new System.Drawing.Point(131, 9);
+            this.lblSelectReader.Name = "lblSelectReader";
+            this.lblSelectReader.Size = new System.Drawing.Size(153, 13);
+            this.lblSelectReader.TabIndex = 15;
+            this.lblSelectReader.Text = "Veuillez sélectionner un lecteur";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(346, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnGetMemory
+            // 
+            this.btnGetMemory.Location = new System.Drawing.Point(49, 157);
+            this.btnGetMemory.Name = "btnGetMemory";
+            this.btnGetMemory.Size = new System.Drawing.Size(75, 23);
+            this.btnGetMemory.TabIndex = 17;
+            this.btnGetMemory.Text = "Get Memory";
+            this.btnGetMemory.UseVisualStyleBackColor = true;
+            this.btnGetMemory.Click += new System.EventHandler(this.btn_Get_Memory_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(49, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Structure 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 351);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(492, 239);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnGetMemory);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblSelectReader);
+            this.Controls.Add(this.btnDF);
+            this.Controls.Add(this.txBxATR);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txBxReponse);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.lblShareMode);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboShareMode);
-            this.Controls.Add(this.comboProtocols);
             this.Controls.Add(this.cbReaders);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -174,19 +168,17 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblShareMode;
-        public System.Windows.Forms.ComboBox comboProtocols;
         public System.Windows.Forms.ComboBox cbReaders;
-        public System.Windows.Forms.ComboBox comboShareMode;
         public System.Windows.Forms.TextBox txBxReponse;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txBxATR;
+        private System.Windows.Forms.Button btnDF;
+        private System.Windows.Forms.Label lblSelectReader;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnGetMemory;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -17,7 +17,8 @@ namespace WindowsFormsApp1
 
         [DllImport("ctacs.dll")]
         static extern int ACSdll();
-        
+        byte[] pbRecvBuffer = new byte[256];
+        SCardError err;
         SCardReader theReader = null;
         public SCardShareMode selShareMode;
         private SCardProtocol selProtocol;
@@ -31,6 +32,11 @@ namespace WindowsFormsApp1
         }
         void readerList()
         {
+            if (cbReaders.SelectedIndex == -1)
+            {
+                lblSelectReader.Visible = true;
+            }
+            else lblSelectReader.Visible = false;
             string[] szReaders = hContext.GetReaders();
             if (szReaders.Length <= 0)
             {
@@ -42,141 +48,215 @@ namespace WindowsFormsApp1
             }
 
         }
+        public void readerConnect()
+        {
+            theReader = new SCardReader(hContext);
+
+             err= theReader.Connect(cbReaders.SelectedItem.ToString(), SCardShareMode.Exclusive, SCardProtocol.T0);
+            CheckError(err);
+            if (err==SCardError.Success)
+            {
+                ATR();
+            }
+        }
+        public void ATR()
+        {
+
+            //Get ATR command
+            byte[] cmd1 = new byte[] { 0x00, 0xBE, 0x00, 0x00, 0x13 };
+
+            err = theReader.Transmit(SCardPCI.T0, cmd1, ref pbRecvBuffer);
+            if (err==SCardError.Success)
+            {
+                string X = "";
+                for (int i = 0; i < pbRecvBuffer.Length; i++)
+                {
+                    X += pbRecvBuffer[i].ToString("X2") + "-";
+                }
+                txBxATR.Text = X;
+                Font F = DefaultFont;
+                txBxATR.Width = X.Length*6;
+            }
+        }
+        void CheckError(SCardError Error)
+        {
+            if (Error != SCardError.Success)
+            {
+                throw new PCSCException(Error, SCardHelper.StringifyError(Error));
+            }
+        }
         public void test()
         {
 
-            void CheckError(SCardError Error)
-            {
-                if (Error != SCardError.Success)
-                {
-                    throw new PCSCException(Error, SCardHelper.StringifyError(Error));
-                }
-            }
             try
             {
                 
 
-                SCardReader reader = new SCardReader(hContext);
-
-                SCardError err = reader.Connect(cbReaders.SelectedItem.ToString(), SCardShareMode.Shared, SCardProtocol.T1);
-                CheckError(err);
-                MessageBox.Show("etat connexion lecteur: "+"\n"+err);
+               
                 IntPtr pioSendPci;
-                switch (reader.ActiveProtocol)
-                {
-                    case SCardProtocol.T0:
-                        pioSendPci = SCardPCI.T0;
-                        break;
-                    case SCardProtocol.T1:
-                        pioSendPci = SCardPCI.T1;
-                        break;
-                    default:
-                        throw new PCSCException(SCardError.ProtocolMismatch, "procotol non supporté" + reader.ActiveProtocol.ToString());
-                }
+                
 
-                byte[] pbRecvBuffer = new byte[8];
 
                 //Send select command
-                byte[] cmd1 = new byte[] { 0xFF, 0x82, 0x41, 0x00, 0x06, 0x12, 0x12, 0x12, 0x12, 0x12, 0x12};
+                #region groupe1
+                // byte[] cmd1 = new byte[] { 0x94, 0x1C, 0x00, 0x00, 0x50, 0x74, 0x84, 0x1F, 0x19, 0xBE, 0xF7, 0x3A, 0x88, 0x02, 0x8B, 0x7B, 0x1C, 0xDE, 0xCE, 0xAB, 0x57, 0x57, 0xE3, 0x30, 0xEA, 0x18, 0xCD, 0xC8, 0x9E, 0x6A, 0xBB, 0x96, 0xD0, 0xCC, 0x88, 0x74, 0x3C, 0xD1, 0xF3, 0x5C, 0x66, 0x0C, 0xD3, 0x82, 0xE5, 0x09, 0x9D, 0x65, 0x6A, 0x7F, 0xD8, 0xF2, 0x4D, 0xDD, 0xED, 0x2F, 0x14, 0xA2, 0x5F, 0xED, 0x62, 0x00, 0xF4, 0x99, 0x3D, 0x06, 0x35, 0x20, 0x58, 0xB4, 0x8E, 0x71, 0xE6, 0x4C, 0xE3, 0xD6, 0xD0, 0x69, 0x1B, 0xB0, 0x46, 0x73, 0xAD, 0x02, 0x34 };
+                // byte[] cmd2 = new byte[] { 0x94, 0x1C, 0x00, 0x00, 0x10, 0xD9, 0x81, 0xFE, 0xEF, 0x90, 0x62, 0x67, 0x20, 0xA8, 0x94, 0x32, 0x52, 0xD5, 0xD6, 0x88, 0x33 };
+                //NN NN NN NN NN NN NN NN
+                //94 1E 00 00 27 00 00 00 71
+                // byte[] cmd3 = new byte[] { 0x94, 0x1E, 0x00 , 0x00, 0x27 , 0x00, 0x00, 0x00,  0X71, 0x01 , 0x01 , 0x01 ,0x01,0x01,0x10, 0x10,0x20, 0x02 , 0x50, 0x18, 0x00 , 0x11 , 0x02 , 0x50, 0x01, 0x8E, 0x07, 0x01, 0x69, 0xF5, 0xFD, 0xEA, 0x7E, 0x44, 0x7E, 0xA4, 0x6E, 0x4D, 0x3A, 0x28, 0x69, 0x1E, 0x4D, 0x0D };
 
-                err = reader.Transmit(SCardPCI.T1, cmd1, ref pbRecvBuffer);
+                #endregion
+                #region groupe2
+                byte[] cmd1 = new byte[] { 0x94, 0x1C, 0x00, 0x00, 0x50, 0xE6, 0xF5, 0x95, 0x66, 0x52, 0xF3, 0xE0, 0xFA, 0xC8, 0x29, 0x50, 0xA2, 0xBD, 0x59, 0x68, 0xFE, 0xF6, 0xBD, 0xB2, 0x31, 0x02, 0xAC, 0x0E, 0x39, 0xF5, 0x70, 0x6C, 0xF2, 0xBF, 0x58, 0x88, 0x8E, 0x2F, 0x0D, 0x21, 0x15, 0xBA, 0x67, 0x05, 0xAA, 0xC5, 0x99, 0xC4, 0x42, 0x59, 0xE4, 0x54, 0x73, 0x17, 0xBA, 0xE9, 0x12, 0xCD, 0x96, 0x79, 0xAD, 0x35, 0x0D, 0x73, 0x48, 0x09, 0x4D, 0x11, 0x97, 0x3C, 0x9A, 0x06, 0x1A, 0x67, 0xD7, 0x00, 0xCF, 0xA3, 0xF5, 0x0D, 0xA0, 0x05, 0x86, 0x15, 0x43 };
+                //byte[] cmd2 = new byte[] { 0x94, 0x1C, 0x00, 0x00, 0x10, 0xF7, 0xDB, 0x48, 0x1D, 0x9D, 0x17, 0x11, 0x5D, 0xDE, 0xFC, 0xE7, 0xFE, 0xFF, 0xEC, 0x85, 0x9C };
+                //byte[] cmd3 = new byte[] { 0x94, 0x1E, 0x00, 0x00, 0x27, 0x00, 0x00, 0x00, 0X71, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x02, 0x50, 0x28, 0x00, 0x11, 0x02, 0x50, 0x08, 0x8E, 0x12, 0x08, 0xB5, 0x67, 0x93, 0x07, 0xE8, 0x6A, 0x44, 0x40, 0xE5, 0xA3, 0xDA, 0xBA, 0xDA, 0x2E, 0x69, 0xF6 };
+                #endregion
+
+
+                err = theReader.Transmit(SCardPCI.T0, cmd1, ref pbRecvBuffer);
+
                 CheckError(err);
-
                 string X = "";
                 for (int i = 0; i < pbRecvBuffer.Length; i++)
                 {
                     X += pbRecvBuffer[i].ToString("X2") + "-";
                 }
                 MessageBox.Show("pbRecvBuffer = " + X);
+                //if (err==SCardError.Success)
+                //{
+                //    err = reader.Transmit(SCardPCI.T0, cmd2, ref pbRecvBuffer);
+                //    string Y = "";
+                //    for (int i = 0; i < pbRecvBuffer.Length; i++)
+                //    {
+                //        Y += pbRecvBuffer[i].ToString("X2") + "-";
+                //    }
+                //    MessageBox.Show("pbRecvBuffer = " + Y);
+                //    if (err==SCardError.Success)
+                //    {
+                //        err = reader.Transmit(SCardPCI.T0, cmd3, ref pbRecvBuffer);
+                //        CheckError(err);
+                //        string Z = "";
+                //        for (int i = 0; i < pbRecvBuffer.Length; i++)
+                //        {
+                //            Z += pbRecvBuffer[i].ToString("X2") + "-";
+                //        }
+                //        MessageBox.Show("pbRecvBuffer = " + Z);
+                //    }
+                //    else MessageBox.Show("erreur : "+err.ToString());
+                //}
+                //else
+                //{
+                //    MessageBox.Show("erreur:"+err.ToString());
+                //}
+                
             }
-            catch (Exception ex)
+            catch (PCSCException ex)
             {
 
-                MessageBox.Show("erreur: " + "\n" + ex.Message+"\n"+ex); ;
+                MessageBox.Show("erreur: " + "\n" + ex.Message); ;
             }
         }
 
-        public void prepare()
-        {
-            comboShareMode.DataSource = Enum.GetNames(typeof(SCardShareMode));
-            comboProtocols.DataSource = Enum.GetNames(typeof(SCardProtocol));
-            foreach (var item in Enum.GetNames(typeof(SCardProtocol)))
-            {
-                string txt = "";
-                if (item.Length>txt.Length)
-                {
-                    txt = item;
-                }
-                Font X = comboProtocols.Font;
-                comboProtocols.Width = TextRenderer.MeasureText(txt, X).Width+30;
-            }
-            foreach (var item in Enum.GetNames(typeof(SCardShareMode)))
-            {
-                string txt = "";
-                if (item.Length > txt.Length)
-                {
-                    txt = item;
-                }
-                Font X = comboProtocols.Font;
-                comboShareMode.Width = TextRenderer.MeasureText(txt, X).Width + 30;
-            }
-            
-        }
+
 
        
 
-        private void comboShareMode_SelectedValueChanged(object sender, EventArgs e)
-        {
-           
-            switch (comboShareMode.SelectedIndex)
-            {
-                case 0:
-                    selShareMode = SCardShareMode.Direct;
-                    break;
-                case 1:
-                    selShareMode = SCardShareMode.Exclusive;
-                    break;
-                case 2:
-                    selShareMode = SCardShareMode.Shared;
-                    break;
 
-                default:
-                    break;
-            }
-        }
 
-        private void comboProtocols_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            switch (comboProtocols.SelectedIndex)
-            {
-
-                case 0:
-                    selProtocol = SCardProtocol.Unset;
-                    break;
-                case 1:
-                    selProtocol = SCardProtocol.T0;
-                    break;
-                case 2:
-                    selProtocol = SCardProtocol.T1;
-                    break;
-                case 3:
-                    selProtocol = SCardProtocol.Any;
-                    break;
-                case 4:
-                    selProtocol = SCardProtocol.Raw;
-                    break;
-                case 5:
-                    selProtocol = SCardProtocol.T15;
-                    break;
-
-                default:
-                    break;
-            }
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            test();
+            readerConnect();
+        }
+
+        private void btn_Get_MF_Click(object sender, EventArgs e)
+        {
+            byte[] cmd1 = new byte[] { 0x94, 0xA4, 0x04, 0x00, 0x0B, 0xA0, 0x00, 0x00, 0x02, 0x91, 0xD0, 0x12, 0x00, 0x08, 0x90, 0x01 };
+            err = theReader.Transmit(SCardPCI.T0, cmd1, ref pbRecvBuffer);
+
+            CheckError(err);
+            string X = "";
+            for (int i = 0; i < pbRecvBuffer.Length; i++)
+            {
+                X += pbRecvBuffer[i].ToString("X2") + "-";
+            }
+            MessageBox.Show("result: "+"\n"+X);
+        }
+
+        private void cbReaders_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblSelectReader.Visible = false;
+        }
+        private void structure2()
+        {
+            byte[] cmd1 = new byte[] { 0x94, 0x1C, 0x00, 0x00, 0x50, 0x80, 0x28, 0xB8, 0x79, 0xDC, 0x53, 0xC0, 0x1A, 0x48, 0xFC, 0x8F, 0x24, 0x3B, 0x35, 0x31, 0x85, 0x23, 0xB2, 0xF7, 0x53, 0x83, 0xB7, 0xD4, 0x12, 0x85, 0x93, 0x98, 0x5A, 0xA6, 0x40, 0xBC, 0x47, 0xF2, 0xD7, 0x1C, 0x05, 0x0D, 0xB9, 0xFA, 0xCE, 0x6F, 0x32, 0xF1, 0xC7, 0x9D, 0x47, 0xAB, 0x8C, 0xDF, 0x65, 0x20, 0x5B, 0x1F, 0x31, 0x48, 0xF9, 0x51, 0x72, 0x90, 0x95, 0xED, 0xA7, 0x74, 0x98, 0xA0, 0x1B, 0x7F, 0x62, 0x35, 0x42, 0xE2, 0x4F, 0x47, 0xDD, 0x8B, 0x6E, 0x34, 0xB2, 0x59, 0x74 },
+            cmd2 = new byte[] { 0x94, 0x1C, 0x00, 0x00, 0x10, 0x2D, 0xDA, 0x5A, 0x47, 0xAD, 0xA1, 0x7F, 0xAA, 0x32, 0xE8, 0x56, 0xDF, 0xB6, 0xD8, 0x86, 0x8B },
+            cmd3 = new byte[] { 0x94, 0x1E, 0x00, 0x00, 0x27, 0x00, 0x00, 0x00, 0x71, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x02, 0x50, 0x28, 0x00, 0x11, 0x02, 0x50, 0x02, 0x8E, 0x07, 0x02, 0xBF, 0x45, 0xE3, 0xF3, 0x20, 0x0F, 0x5B, 0xB2, 0xE8, 0xDD, 0xFE, 0x45, 0xCA, 0x09, 0x05, 0xB5 };
+            err = theReader.Transmit(SCardPCI.T0, cmd1, ref pbRecvBuffer);
+            string X = "";
+            for (int i = 0; i < pbRecvBuffer.Length; i++)
+            {
+                X += pbRecvBuffer[i].ToString("X2") + "-";
+            }
+            MessageBox.Show("result: " + "\n" + X);
+            if (err==SCardError.Success)
+            {
+                err = theReader.Transmit(SCardPCI.T0, cmd2, ref pbRecvBuffer);
+                string Y = "";
+                for (int i = 0; i < pbRecvBuffer.Length; i++)
+                {
+                    Y += pbRecvBuffer[i].ToString("X2") + "-";
+                }
+                MessageBox.Show("result: " + "\n" + Y);
+                if (err==SCardError.Success)
+                {
+                    err = theReader.Transmit(SCardPCI.T0, cmd3, ref pbRecvBuffer);
+                    string z = "";
+                    for (int i = 0; i < pbRecvBuffer.Length; i++)
+                    {
+                        z += pbRecvBuffer[i].ToString("X2") + "-";
+                    }
+                    MessageBox.Show("result: " + "\n" + z);
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] cmd = new byte[] { 0x94, 0xA4, 0x04, 0x00, 0x0B, 0xA0, 0x00, 0x00, 0x02, 0x91, 0xD0, 0x12, 0x00, 0x08, 0x90, 0x01 };
+            byte[] cmd2 = new byte[] { 0x6F, 0x25, 0x84, 0x0B, 0xA0, 0x00, 0x00, 0x02, 0x91, 0xD0, 0x12, 0x00, 0x08, 0x90, 0x01, 0xA5, 0x16, 0xBF, 0x0C, 0x13, 0xC7, 0x08, 0x00, 0x00, 0x00, 0x00, 0x55, 0x66, 0x77, 0x88, 0x53, 0x07, 0x06, 0x0A, 0x01, 0x02, 0x20, 0x03, 0x11, 0x90, 0x00};
+            err = theReader.Transmit(SCardPCI.T0, cmd, ref pbRecvBuffer);
+
+            CheckError(err);
+            if (err==SCardError.Success)
+            {
+                err = theReader.Transmit(SCardPCI.T0, cmd2, ref pbRecvBuffer);
+                string X = "";
+                for (int i = 0; i < pbRecvBuffer.Length; i++)
+                {
+                    X += pbRecvBuffer[i].ToString("X2") + "-";
+                }
+                MessageBox.Show("result: " + "\n" + X);
+
+            }
+        }
+
+        private void btn_Get_Memory_Click(object sender, EventArgs e)
+        {
+            byte[] cmd = new byte[] { 0x94, 0xD2, 0x01, 0x14, 0x16, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0xFA};
+            pbRecvBuffer = new byte[02];
+            err = theReader.Transmit(SCardPCI.T0, cmd, ref pbRecvBuffer);
+            CheckError(err);
+            string Y = "";
+            for (int i = 0; i < pbRecvBuffer.Length; i++)
+            {
+                Y += pbRecvBuffer[i].ToString("X2") + "/";
+            }
+            MessageBox.Show("résultat de la lecture EEPROM: "+"\n"+Y);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            structure2();
         }
     }
 }
